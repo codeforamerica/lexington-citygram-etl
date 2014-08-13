08685cc18aa1f12a9b37175c8659a445a5e5cf81
Erik Schwartz <erik@erikschwartz.net>
2014-08-13 14:37:49 -0700

    Update feeds

--- etl.sh
+++ etl.sh
@@ -1,5 +1,5 @@
 function etl_building_permits {
-  echo 'geocode'
+  echo 'geocode building permits'
   ruby geocode_addresses.rb building-permits-2014.csv > building-permits-geocoded-2014.csv
 
   echo 'load into postgres'
@@ -7,11 +7,11 @@ function etl_building_permits {
   csvsql --db postgresql:///geocode_code_enforcement --insert --table building_permits building-permits-geocoded-2014.csv
 
   echo 'generate geojson'
-  rake generate_geojson:building_permits
+  rake generate_geojson:building_permits > building_permits.json
 }
 
 function etl_code_enforcement {
-  echo 'geocode'
+  echo 'geocode code enforcement'
   ruby geocode_addresses.rb code-enforcement-2014.csv > code-enforcement-geocoded-2014.csv
 
   echo 'load into postgres'
@@ -19,7 +19,7 @@ function etl_code_enforcement {
   csvsql --db postgresql:///geocode_code_enforcement --insert --table code_enforcement code-enforcement-geocoded-2014.csv
 
   echo 'generate geojson'
-  rake generate_geojson:code_enforcement
+  rake generate_geojson:code_enforcement > code_enforcement.json
 }
 
 etl_building_permits
